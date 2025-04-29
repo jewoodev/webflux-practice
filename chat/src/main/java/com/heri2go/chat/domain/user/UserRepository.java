c38b5933759dc3bebd1ba6d0e76b3f12c1d5c9b1
jewoodev <jewoos15@naver.com>
2025-04-29 12:59:33 +0900

    🚑 fix: 또다시 확인된 2회 chain.filter 수행 문제 fix (정리가 필요한 코드들을 정리, 검증 및 인증의 책임을 JWT 필터에 모두 전가)

--- chat/src/main/java/com/heri2go/chat/domain/user/UserRepository.java
+++ chat/src/main/java/com/heri2go/chat/domain/user/UserRepository.java
@@ -1,10 +1,12 @@
 package com.heri2go.chat.domain.user;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
 import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
+
+    @Cacheable(value = "user", key = "#username", cacheManager = "cacheManager")
     Mono<User> findByUsername(String username);
 
     Mono<User> findByEmail(String email);
