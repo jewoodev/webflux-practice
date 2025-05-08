9a3caaf76ba18ce43c3f9571b7c53cf4d8e76ed8
jewoodev <jewoos15@naver.com>
2025-05-08 10:51:53 +0900

    🚑 fix: 캐싱에 password 제외하자 로그인 요청 검증에서 사용하지 못하는 문제

--- chat/src/main/java/com/heri2go/chat/web/service/user/response/UserResponse.java
+++ chat/src/main/java/com/heri2go/chat/web/service/user/response/UserResponse.java
@@ -1,22 +1,23 @@
 package com.heri2go.chat.web.service.user.response;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.heri2go.chat.domain.user.Role;
 import com.heri2go.chat.domain.user.User;
 import lombok.Builder;
 
 @Builder
 public record UserResponse(
-        @JsonIgnore String id,
+        String id,
         String username,
-        @JsonIgnore String password,
+        String password,
         String email,
         Role role
 ) {
     public static UserResponse from(User user) {
         return UserResponse.builder()
+                .id(user.getId())
                 .username(user.getUsername())
                 .password(user.getPassword())
+                .email(user.getEmail())
                 .role(user.getRole())
                 .build();
     }
